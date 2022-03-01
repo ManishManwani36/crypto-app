bc46c39a3b473ed1689ae1e07212e69cb7561750
ManishManwani36 <64730408+ManishManwani36@users.noreply.github.com>
2022-03-01 16:11:17 +1030

    built CoinSurf

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Coins from "./components/Coins";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Coin from "./routes/Coin";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Coins coins={coins} />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
